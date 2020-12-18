551d06bf036b36d0978522eb8e3d74da4342761d
abhisheknaiidu <abhisheknaiidu@users.noreply.github.com>
2020-12-18 10:11:02 +0000

    Prettified Code!

--- src/main.js
+++ src/main.js
@@ -12,5 +12,5 @@ Vue.config.productionTip = false;
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: (h) => h(App),
 }).$mount("#app");
--- src/store/index.js
+++ src/store/index.js
@@ -9,5 +9,5 @@ export default new Vuex.Store({
   state: {},
   mutations: {},
   actions: {},
-  modules: {}
+  modules: {},
 });
